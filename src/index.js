67f4063028e80cd0c457740ecf6f9681de20813f
biedn001 <biedn001@umn.edu>
2018-09-12 14:48:19 -0500

    ICONS!!! And icon switch

--- src/index.js
+++ src/index.js
@@ -10,10 +10,8 @@ class Weather extends React.Component {
             items: {}
         };
     }
-
     componentDidMount() {
         fetch("http://api.openweathermap.org/data/2.5/weather?q=minneapolis&units=imperial&appid=67975210d69828dd5fa681b6216543d0")
-        //fetch("https://jsonplaceholder.typicode.com/todos/1")
             .then(res => res.json())
             .then(
                 (result) => {
@@ -30,7 +28,34 @@ class Weather extends React.Component {
                 }
             )
     }
-
+    getIcon(description) {
+        switch (description) {
+            case "01d":
+            case "01n":
+            default:
+                return "Sunny.png";
+            case "02d":
+            case "02n":
+            case "03d":
+            case "03n":
+                return "PartlyCloudy.png";
+            case "04d":
+            case "04n":
+            case "50d":
+            case "50n":
+                return "Cloudy.png";
+            case "09d":
+            case "09n":
+            case "10d":
+            case "10n":
+            case "11d":
+            case "11n":
+                return "Rainy.png";
+            case "13d":
+            case "13n":
+                return "Snowy.png";
+        }
+    }
     render() {
         const { error, isLoaded, items } = this.state;
 
@@ -44,7 +69,10 @@ class Weather extends React.Component {
             return(
                 //The real Weather block code
                 <div className="col-md-6">
-                    { items.main.temp }
+                    <h1>{ items.name }</h1>
+                    <h2>{ items.weather[0].description }</h2>
+
+                    <img src={ this.getIcon( items.weather[0].icon ) } alt="Weather Icon"></img>
                 </div>
             );
         }
