770bc3c08b6a5cbba79f116b54ab6fdbb81e1c6e
Alex Biedny <biedn001@umn.edu>
2018-09-12 11:45:05 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Weather extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state  ={
+            error: null,
+            isLoaded: false,
+            items: []
+        };
+    }
+
+    componentDidMount() {
+        fetch("api.openweathermap.org/data/2.5/weather?q=minneapolis&units=imperial&appid=67975210d69828dd5fa681b6216543d0")
+        //fetch("https://jsonplaceholder.typicode.com/todos/1")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    console.log(result);
+                    this.setState({
+                        isLoaded: true,
+                        items: result
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
+
+    render() {
+        const { error, isLoaded, items } = this.state;
+
+        if (error) {
+            return <div>Error: {error.message}</div>
+        }
+        else if (!isLoaded) {
+            return <div>Loading...</div>
+        }
+        else {
+            return(
+                //The real Weather block code
+                <div className="col-md-6">
+
+                </div>
+            );
+        }
+    }
+}
+
+ReactDOM.render(
+    <Weather/>,
+    document.getElementById("root")
+);
